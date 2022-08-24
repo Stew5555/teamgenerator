5bf7b63fcf374a5d0d6202571ad035765834e96f
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-08-23 22:04:43 -0700

    update

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -2,18 +2,18 @@ const engineer = require("../lib/engineer");
 
 test("Can set GitHUb account via constructor", () => {
   const test = "GitHubUser";
-  const e = new engineer("Foo", 1, "test@test.com", test);
+  const e = new engineer("github", 1, "test@test.com", test);
   expect(e.github).toBe(test);
 });
 
 test("getRole() should return \"Engineer\"", () => {
   const test = "Engineer";
-  const e = new engineer("Foo", 1, "test@test.com", "GitHubUser");
+  const e = new engineer("career", 1, "test@test.com", "GitHubUser");
   expect(e.getRole()).toBe(test);
 });
 
 test("Can get GitHub username via getGithub()", () => {
   const test = "GitHubUser";
-  const e = new engineer("Foo", 1, "test@test.com", test);
+  const e = new engineer("userName", 1, "test@test.com", test);
   expect(e.getGithub()).toBe(test);
 });
