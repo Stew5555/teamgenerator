1647149e793971a324ca27f2f051626fe86ef88d
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-08-23 17:30:44 -0700

    update5

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,7 +1,26 @@
 
+const { default: test } = require("node:test");
 const employee = require("../lib/employee") 
 
 test("creates a new empoyee", () => { 
     const e = new employee();
-    exportAllDeclaration(typeof(e)).toBe('object');
+    expect(typeof(e)).toBe('object');
+})
+
+test("sets a name", () => {
+    const name = 'stew';
+    const e = new employee(name);
+    expect(e.name).toBe(name);
+});
+
+test("sets an ID for the employee", () => {
+    const num = 123;
+    const e = new employee('item', num);
+    expect(e.id).toBe(num);
+});
+
+test("sets an email", () => {
+    const testEmail = "testEmail@something.come"
+    const e = new employee("email", 1, testEmail);
+    expect(e.email).toBe(testEmail)
 })
