51b2b74d19fbec68327967459d9a799c1cd25f70
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-08-23 17:53:00 -0700

    update5

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,5 +1,5 @@
 
-const { default: test } = require("node:test");
+// const { default: test } = require("node:test");
 const employee = require("../lib/employee") 
 
 test("creates a new empoyee", () => { 
@@ -20,7 +20,31 @@ test("sets an ID for the employee", () => {
 });
 
 test("sets an email", () => {
-    const testEmail = "testEmail@something.come"
-    const e = new employee("email", 1, testEmail);
-    expect(e.email).toBe(testEmail)
+    const test = "testEmail@something.come"
+    const e = new employee("email", 1, test);
+    expect(e.email).toBe(test)
 })
+
+test("Can get name via getName()", () => {
+    const test = "stew";
+    const e = new employee(test);
+    expect(e.getName()).toBe(test);
+  });
+  
+  test("Can get id via getId()", () => {
+    const test = 100;
+    const e = new employee("email", test);
+    expect(e.getId()).toBe(test);
+  });
+  
+  test("Can get email via getEmail()", () => {
+    const test = "test@test.com";
+    const e = new employee("email", 1, test);
+    expect(e.getEmail()).toBe(test);
+  });
+  
+  test("getRole() should return \"employee\"", () => {
+    const test = "Employee";
+    const e = new employee("stew", 1, "test@test.com");
+    expect(e.getRole()).toBe(test);
+  });
