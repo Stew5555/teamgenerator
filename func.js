e0ff20f53211729fd3e9b5ecdba2caca623db656
Stew5555 <104962519+Stew5555@users.noreply.github.com>
2022-08-23 18:52:57 -0700

    update8

--- func.js
+++ func.js
@@ -1,6 +1,86 @@
 //node
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
 const fs = require("fs");
-const team = require("./codeStyle/index")
+const team = require("./codeStyle/index");
 //lib files
-const engineer = require("./lib/engineer")
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib//intern");
+const Manger = require("./lib/manager");
+
+
+const newEmployeeInfo = [];
+
+//questtions for users
+const questions = async () => {
+    const answers = await inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is your name?",
+          name: "name",
+        },
+        {
+          type: "input",
+          message: "What is your ID number?",
+          name: "id",
+        },
+        {
+          type: "input",
+          message: "What is your email?",
+          name: "email",
+        },
+        {
+          type: "list",
+          message: "What is your role?",
+          name: "role",
+          choices: ["Engineer", "Intern", "Manager"],
+        },
+      ])
+  
+  
+      
+      //  console.log(answers);
+        // if manager selected, answer these specific question
+        if (answers.role === "Manager") {
+          const managerAnsewrs = await inquirer
+            .prompt([
+              {
+                type: "input",
+                message: "What is your office number",
+                name: "officeNumber",
+              },
+            ])
+            const newManger = new Manger(
+                answers.name,
+                answers.id,
+                answers.email,
+                managerAnsewrs.officeNumber
+            )
+            newEmployeeInfo.push(newManger);
+
+
+        }else if (answers.role ==="Engineer") {
+            const githubAnsewrs = await inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is your GitHub user name?",
+                    name: "github",
+                  }
+                ])
+                  const newEngineer = new Engineer(
+                    answers.name,
+                    answers.id,
+                    answers.email,
+                    githubAns.github
+                  );
+                  newStaffMemberData.push(newEngineer);
+
+                }
+            
+        }
+
+
+    
+
+};
